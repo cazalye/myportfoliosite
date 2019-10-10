49793f9c59b2a2d8e9bfd7f0ae45e13ce6d77eea
Emma Cazalye <e.cazaly7@gmail.com>
2019-10-10 07:45:39 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Layout, Navigation, Header, Drawer, Content} from 'react-mdl';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+        </Content>
+    </Layout>
+</div>
+  );
+}
+
+export default App;
