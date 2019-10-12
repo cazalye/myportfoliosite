26b0fe68158b051f809fbb6553d2d12396d098f5
Emma <e.cazaly7@gmail.com>
2019-10-12 09:25:06 +0300

    Add landing page content and style

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,7 +5,29 @@ class Landing extends Component {
         return(
             <div style={{width: '100%', margin:'auto'}}>
                 <Grid className='landing-grid'>
-                    <Cell col={12}>hello</Cell>
+                    <Cell col={12}>
+                        <img 
+                        src="https://previews.123rf.com/images/jemastock/jemastock1706/jemastock170619912/81143751-male-traveler-or-tourist-avatar-with-camera-icon-image-vector-illustration-design.jpg" 
+                        alt="travel avatar"
+                        className="avatar-img"/>
+                        
+                        <div className="banner-text">
+                            <h1>Front End Web Developer</h1>
+                            <hr/>
+                            <p> HTML/CSS | Bootstrap | JavaScript | React</p>
+                            <div className="social-links"> 
+
+                                <a href="htps://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin" aria-hidden="true" />
+                                </a>
+
+                                <a href="htps://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github" aria-hidden="true" />
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
                 </Grid>    
             </div>
         )
