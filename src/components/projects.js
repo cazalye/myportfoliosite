192752d1496fe18c0449d516d1e3eb44624603c0
Emma <e.cazaly7@gmail.com>
2019-10-14 09:12:04 +0300

    Add WIP tabs to projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
+import {Tabs, Tab} from 'react-mdl';
 class Projects extends Component {
+    constructor (props){
+        super(props);
+        this.state = {activeTab:0};
+    }
+
     render() {
         return(
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab:tabId })} ripple>
+                    <Tab> Project1 </Tab>
+                    <Tab> Project2 </Tab>
+                    <Tab> Project3 </Tab>
+                    <Tab> Project4 </Tab>
+                </Tabs>
+                {/* <section className="projects-grid">
+                    {this.toggleCategories()}
+                </section> */}
+            </div>
         )
     }
 }
