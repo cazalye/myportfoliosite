66a1cc8ce31e267ca07e9faf9b94e98ec96e5c76
Emma <e.cazaly7@gmail.com>
2019-10-16 09:28:09 +0300

    Add initial contact content and style

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 class Contact extends Component {
     render() {
         return(
-            <div><h1>Contact Page</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col="6">
+                        <h2>Emma Cazaly</h2>
+                        <img src="img/Me.jpg" alt="me" style={{height:'250px'}}/>
+                        <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}>ipsum lorem dolor sit</p>
+                    </Cell>
+                    <Cell col="6">
+                        <h2>Contact Me</h2>
+                        <hr/>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
