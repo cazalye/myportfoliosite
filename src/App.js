dcc286c87fb0334a588a3c4a031f17d75275f293
Filippo Zonta <kknull0@gmail.com>
2019-10-21 08:41:55 +0300

    Fix Drawer links not closing sidebar

--- src/App.js
+++ src/App.js
@@ -5,33 +5,39 @@ import './App.css';
 import Main from './components/main';
 import {Link} from 'react-router-dom';
 
-function App() {
-  return (
-<div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link to="/" className="header-title"> Emma Cazaly </Link>} scroll>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link to="/" className="header-title-drawer"> Emma Cazaly </Link>}>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
-  );
+class App extends React.Component {
+    hideToggle() {
+        var selectorId = document.querySelector('.mdl-layout');
+        selectorId.MaterialLayout.toggleDrawer();
+    }
+    render() {
+        return (
+            <div className="demo-big-content">
+                <Layout>
+                    <Header className="header-color" title={<Link to="/" className="header-title"> Emma Cazaly </Link>} scroll>
+                        <Navigation>
+                            <Link to="/resume">Resume</Link>
+                            <Link to="/aboutme">About Me</Link>
+                            <Link to="/projects">Projects</Link>
+                            <Link to="/contact">Contact</Link>
+                        </Navigation>
+                    </Header>
+                    <Drawer title={<Link to="/" onClick={() => this.hideToggle()} className="header-title-drawer"> Emma Cazaly </Link>}>
+                        <Navigation>
+                            <Link onClick={() => this.hideToggle()} to="/resume">Resume</Link>
+                            <Link onClick={() => this.hideToggle()} to="/aboutme">About Me</Link>
+                            <Link onClick={() => this.hideToggle()} to="/projects">Projects</Link>
+                            <Link onClick={() => this.hideToggle()} to="/contact">Contact</Link>
+                        </Navigation>
+                    </Drawer>
+                    <Content>
+                        <div className="page-content" />
+                        <Main/>
+                    </Content>
+                </Layout>
+            </div>
+        )
+    }
 }
 
 export default App;
