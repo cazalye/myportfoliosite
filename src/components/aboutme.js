be5ef262b98fa99a1d56165c30cac6af0fce3557
Emma <e.cazaly7@gmail.com>
2019-10-23 16:36:12 +0300

    Add text, photo and styling to about page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 class About extends Component {
     render() {
         return(
-            <div><h1>About Page</h1></div>
+            <Grid>
+                <Cell col={12}>
+                   <div className="aboutme-background">
+                   </div>
+                </Cell>
+                <Cell col={6}>   
+                    <div className="aboutme-text">
+                        <h1>About Me</h1>
+                        <p>I am a designer and front-end developer who creates evocative websites and stationery, centered around adventure, conscious living and environmental awareness.</p>
+                    </div>
+                </Cell>
+            </Grid>
+
         )
     }
 }
