f81da855b2c40ed870c553ed0e170069266d7c3b
Emma <e.cazaly7@gmail.com>
2019-10-24 10:14:43 +0300

    Change background image on landing page. Add Responsive styling

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,25 +6,17 @@ class Landing extends Component {
             <div style={{width: '100%', margin:'auto'}}>
                 <Grid className='landing-grid'>
                     <Cell col={12}>
-                        <img 
-                        src="https://image.shutterstock.com/image-vector/woman-working-laptop-character-260nw-1044466321.jpg"
-                        alt="female design avatar"
-                        className="avatar-img"/>
-                        
                         <div className="banner-text">
                             <h1>Web Designer & Front-end Developer</h1>
                             <hr/>
                             <p> HTML/CSS | JavaScript | React | Shopify? | Adobe Suite | Photography </p>
                             <div className="social-links"> 
-
                                 <a href="htps://google.com" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-linkedin" aria-hidden="true" />
                                 </a>
-
                                 <a href="htps://google.com" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-github" aria-hidden="true" />
                                 </a>
-
                             </div>
                         </div>
                     </Cell>
