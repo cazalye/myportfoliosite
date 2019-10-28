f48331e73d799d521a3996f144e52757e3db0ba3
Emma <e.cazaly7@gmail.com>
2019-10-28 07:25:58 +0200

    Style contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,20 +1,18 @@
 import React, {Component} from 'react';
-import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 class Contact extends Component {
     render() {
         return(
-            <div className="contact-container">
-                <div className="left-box">
-                    <img src="/img/Me_portrait.jpg" alt="me" style={{height:'320px'}}/>
+            <div className="contact-background">
+                <div className="contact-container">
+                    <div className="contact-content">
+                        <h2>Contact Me </h2>
+                        <hr/>
+                        <h5> <strong>Address:</strong> Hameentie 27D 95</h5>
+                        <h5> <strong>Phone:</strong> +358 417077974</h5>
+                        <h5> <strong>Email:</strong> e.cazaly7@gmail.com </h5>
+                        <h5> <strong>Web:</strong> cazalye.com</h5>
+                    </div>
                 </div>
-                <div className="right-box">
-                    <h2>Contact Me </h2>
-                    <h5> <strong>Address:</strong> Hameentie 27D 95</h5>
-                    <h5> <strong>Phone:</strong> +358 417077974</h5>
-                    <h5> <strong>Email:</strong> e.cazaly7@gmail.com </h5>
-                    <h5> <strong>Web:</strong> cazalye.com</h5>
-                </div>
-                <div className="contact-edge"/>
             </div>
         )
     }
