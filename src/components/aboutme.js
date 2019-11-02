731264e145a449585dd05932213dd62cc817b97c
Emma <e.cazaly7@gmail.com>
2019-11-02 12:11:31 +0200

    Fix style in landing page and about

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -10,14 +10,14 @@ class About extends Component {
                 </Cell>
                 <Cell col={6}>   
                     <div className="aboutme-text">
-                        <h1>About Me</h1>
-                        <p>I'm a designer and front-end developer with a passion for photography and travel. </p>
-                        <hr/>
+                        {/* <h1>About Me</h1> */}
+                        <h2>I'm a designer and front-end developer with a passion for photography and travel. </h2>
+                        {/* <hr/> */}
                         <p>I create evocative websites, catchy copy and inspiring stationery, centered around adventure, conscious living and environmental awareness. </p>
-                        <hr/>
+                        {/* <hr/> */}
                         <p> I hold both Australian and Italian citzenship.</p>
-                        <hr/>
-                        <a href="https://www.cazalye.com">Visit me at <strong>cazalye.com</strong></a>
+                        {/* <hr/> */}
+                        <p>Visit me at <a href="https://www.cazalye.com">cazalye.com</a></p> 
                     </div>
                 </Cell>
             </Grid>
