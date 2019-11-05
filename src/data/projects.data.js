58da426e211be5d298cafc04a8c1615b4dbcef6a
Emma <e.cazaly7@gmail.com>
2019-11-05 08:17:26 +0200

    Add data and styling to Projects page

--- src/data/projects.data.js
+++ src/data/projects.data.js
@@ -13,7 +13,7 @@ export const Categories = [
                     },
                     {
                         link: "https://cazalye.com/portfolio",
-                        icon: "fas   fa-globe-americas",
+                        icon: "fa fa-globe",
                     },
                 ]
             },
@@ -23,23 +23,27 @@ export const Categories = [
                 text: "Created with React",
                 icons: [
                     {
-                        text: "GitHub"
+                        link: "https://github.com/cazalye/cazalye",
+                        icon: "fab fa-github",
                     },
                     {
-                        text: "cazalye.com/travel"
+                        link: "https://cazalye.com/travel",
+                        icon: "fa fa-globe",
                     },
                 ]
             },
             {
                 image: "./img/projects/placeholder_screenshot.png",
                 title: "E-commerce", 
-                text: "Created with Shopify / Woo-Commerce?",
+                text: "Created with Shopify",
                 icons: [
                     {
-                        text: "GitHub"
+                        link: "https://github.com/cazalye/shop",
+                        icon: "fab fa-github",
                     },
                     {
-                        text: "cazalye.com/shop"
+                        link: "https://cazalye.com/shop",
+                        icon: "fa fa-globe",
                     },
                 ]
             },
@@ -49,14 +53,160 @@ export const Categories = [
         name: "design",
         projects: [
             {
-                title: "design Website",
-                text: "Created using React<br/>cazalye.com/portfolio",
+                image: "./img/projects/placeholder_screenshot.png",
+                title: "Personal Website",
+                text: "Created with Adobe XD",
                 icons: [
                     {
-                        text: "GitHub"
+                        // ADOBE XD FILE
+                        link: "",
+                        icon: "fab fa-adobe",  
+                    },
+                    {
+                        link: "https://cazalye.com",
+                        icon: "fa fa-globe",
+                    },
+                ]
+            },
+            {
+                image: "./img/projects/placeholder_screenshot.png",
+                title: "Logo",
+                text: "Created with Adobe Illustrator",
+                icons: [
+                    {
+                        // PDF
+                        link: "",
+                        icon: "fas fa-file-pdf",  
+                    },
+                ]
+            },
+            {
+                image: "./img/projects/placeholder_screenshot.png",
+                title: "Gratitude Journal",
+                text: "Created with Adobe InDesign",
+                icons: [
+                    {
+                        // PDF
+                        link: "",
+                        icon: "fas fa-file-pdf",  
+                    },
+                    {
+                        // PHOTOS
+                        link: "",
+                        icon: "fas fa-file-image",  
+                    },
+                ]
+            },
+            {
+                image: "./img/projects/placeholder_screenshot.png",
+                title: "Eco-friendly Postcards",
+                text: "Created with Adobe Lightroom & InDesign",
+                icons: [
+                    {
+                        // PDF
+                        link: "",
+                        icon: "fas fa-file-pdf",  
+                    },
+                    {
+                        // PHOTOS
+                        link: "",
+                        icon: "fas fa-file-image",
                     }
                 ]
             }
         ]
-    }
+    },
+    {
+        name: "Copy",
+            projects: [
+                {
+                    image: "./img/projects/placeholder_screenshot.png",
+                    title: "Travel Blog",
+                    text: "Wordpress Blog",
+                    icons: [
+                        {
+                            // blog posts
+                            link: "https://cazalye.com/trave;/post1",
+                            icon: "fab fa-wordpress",  
+                        },
+                        {
+                            link: "https://cazalye.com/travel/post2",
+                            icon: "fab fa-wordpress",
+                        },   
+                        {
+                            link: "https://cazalye.com/travel/post3",
+                            icon: "fab fa-wordpress",
+                        },   
+                    ]
+                },
+                {
+                    image: "./img/projects/placeholder_screenshot.png",
+                    title: "Gratitude Journal",
+                    text: "About page, promotional copy",
+                    icons: [
+                        {
+                            // Snapshot about page
+                            link: "",
+                            icon: "fas fa-file-pdf",  
+                        },
+                        {
+                            // Snaphot promotional text
+                            link: "",
+                            icon: "fas fa-file-pdf",
+                        },    
+                    ]
+                },
+            ]
+    },
+    {
+        name: "Photogrpahy",
+            projects: [
+                {
+                    image: "./img/projects/placeholder_screenshot.png",
+                    title: "Film Photography",
+                    text: "Shot with Canon AE-1",
+                    icons: [
+                        {
+                            // Top 5 images --> LINK to gallery on separate page
+                            link: "",
+                            icon: "fa fa-link",  
+                        }, 
+                    ]
+                },
+                {
+                    image: "./img/projects/placeholder_screenshot.png",
+                    title: "Digital Photography",
+                    text: "Shot with Sony a7 and vintage lenses",
+                    icons: [
+                        {
+                             // Top 5 images --> LINK to gallery on separate page
+                            link: "",
+                            icon: "fa fa-globe",  
+                        },   
+                    ]
+                },
+                {
+                    image: "./img/projects/placeholder_screenshot.png",
+                    title: "Lightroom Presets",
+                    text: "Autumn, Canyon, Mountains",
+                    icons: [
+                        {
+                             // Top 3 LR presets, before-after slider
+                            link: "",
+                            icon: "fa fa-link",  
+                        },   
+                        {
+                             // Top 3 LR presets, before-after slider
+                            link: "",
+                            icon: "fa fa-link",  
+                        },   
+                        {
+                             // Top 3 LR presets, before-after slider
+                            link: "",
+                            icon: "fa fa-link",  
+                        },   
+                    ]
+                },
+            ]
+    },
 ];
